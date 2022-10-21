414305bec47268afdb80cc1a036648a5e27e55ec
Vageta12 <115899955+Vageta12@users.noreply.github.com>
2022-10-21 13:51:47 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,23 @@
 import  os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+
+
+
+BOT_TOKEN = os.environ.get("5551715711:AAFZOHiS6EIEM29wafp2gjFBI0nmXic61lY")
+
+APP_ID = int(os.environ.get("7088245"))
+
+API_HASH = os.environ.get("de7aad28ad0962763dd102464369af18")
+
+
+
 
 youtube_next_fetch = 0  # time in minute
+
+
+
+
+
 
 
 EDIT_TIME = 5
